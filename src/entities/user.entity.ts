784d9ba07a90bca003e107fb84a2d54eb493b08f
Minh Hung <nmhung.7676@gmail.com>
2023-02-24 09:35:19 +0700

    update nullable for username entity

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -5,7 +5,7 @@ export class UserEntity {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @Column({ unique: true, nullable: true })
+  @Column({ unique: true, nullable: false })
   username: string;
 
   @Column()
